492263a11df2cb775f0497170e6fb8217bf6b556
villamizarA <99750370+villamizarA@users.noreply.github.com>
2023-06-21 16:15:34 -0300

    modif

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-const navLinks = document.querySelectorAll('.nav-links');
 
+const navLinks = document.querySelectorAll('.nav-links');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
 navLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    const currentLink = document.querySelector('.active');
 
-   link.addEventListener('click', function (e) {
-    const currentLink =document.querySelector("active");
-
-    currentLink.classList.remove("active");
-    e.target.classList.add("active")
+    currentLink.classList.remove('active');
+    e.target.classList.add('active');
   });
+});
+
+hamburger.addEventListener('click', function () {
+  navMenu.classList.toggle('show');
 });
